999081d5c91739ac1e6b9d429fcd7dadabc27230
chinnarao <chinnarao@live.com>
2018-08-30 02:04:42 -0700

    npm install angular/flex-layout-builds --save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { MdcButtonModule, MdcFabModule, MdcIconModule, } from '@angular-mdc/web';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { MdcButtonModule, MdcFabModule, MdcIconModule, } from '@angular-mdc/web'
     MdcButtonModule,
     MdcFabModule,
     MdcIconModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
