924afde35b2ac988eff27c83018163312bcd0263
chinnarao <chinnarao@live.com>
2018-08-29 21:40:51 -0700

    trimox/angular-mdc-web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MdcButtonModule, MdcFabModule, MdcIconModule, } from '@angular-mdc/web';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MdcButtonModule,
+    MdcFabModule,
+    MdcIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
