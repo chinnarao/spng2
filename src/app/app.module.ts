858bddd46f69b8a88c0b9ebcb8857ba74b730cf4
chinnarao <chinnarao@live.com>
2018-10-22 16:38:25 -0700

    code clean

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import {HttpModule } from '@angular/http';
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule, ErrorHandler} from '@angular/core';
 import {FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -6,14 +5,12 @@ import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {SharedModule } from './shared/shared.module';
-import {MdcModule } from './shared/modules/mdc.module';  // this is not required but , app.component.html is using for some reason.
-import {FirebaseModule } from './menu/firebase.module';
 import {LoggerModule, NgxLoggerLevel, NGXLogger } from 'ngx-logger';
 import {AppGlobalErrorhandler } from './_error/app.global.errorhandler';
 import {HttpClientModule } from '@angular/common/http';
 import {HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import {InMemoryDataService } from './_in-memory/in-memory-data.service';
-import { ApiService } from './_api/api.service';
+import { MenuMdcModule } from './menu/menu.mdc.module'; // this is not required but , app.component.html is using for some reason.
 import { MenuModule } from './menu/menu.module';
 
 @NgModule({
@@ -21,13 +18,13 @@ import { MenuModule } from './menu/menu.module';
     AppComponent,
   ],
   imports: [
-    BrowserModule, AppRoutingModule, FlexLayoutModule, FormsModule, ReactiveFormsModule, HttpModule, HttpClientModule,
-    MdcModule, MenuModule, FirebaseModule,
+    BrowserModule, AppRoutingModule, FlexLayoutModule, FormsModule, ReactiveFormsModule, HttpClientModule,
+    MenuMdcModule, MenuModule,
     LoggerModule.forRoot({serverLoggingUrl: '/api/logs', level: NgxLoggerLevel.INFO, serverLogLevel: NgxLoggerLevel.OFF}),
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false }),
     SharedModule,
   ],
-  providers: [NGXLogger, ApiService,
+  providers: [NGXLogger,
     {provide: ErrorHandler, useClass: AppGlobalErrorhandler}
   ],
   bootstrap: [AppComponent]
--- src/app/menu/menu.module.ts
+++ src/app/menu/menu.module.ts
@@ -1,20 +1,62 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { ReactiveFormsModule } from '@angular/forms';
-import { WebStorageModule } from 'ngx-store';
+import { CommonModule } from '@angular/common';
+import {environment} from 'src/environments/environment';
+import {FirebaseUIModule, firebase, firebaseui} from 'firebaseui-angular';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import { LogInComponent } from './log-in/log-in.component';
+import { HeaderComponent } from './header/header.component';
+import { RegisterComponent } from './register/register.component';
 import { MenuMdcModule } from './menu.mdc.module';
-import { FirebaseModule } from './firebase.module';
+import { NgMatSearchBarModule } from 'ng-mat-search-bar';
 import { MenuService } from './menu.service';
+
+export const firebaseUiAuthConfig: firebaseui.auth.Config = {
+  signInFlow: 'popup',
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    {
+      scopes: [
+        'public_profile',
+        'email',
+        'user_likes',
+        'user_friends'
+      ],
+      customParameters: {
+        'auth_type': 'reauthenticate'
+      },
+      provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID
+    },
+    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    {
+      requireDisplayName: false,
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID
+    },
+    firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+    firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+  ],
+  tosUrl: 'https://www.google.com',
+  privacyPolicyUrl: 'https://www.google.com',
+  credentialHelper: firebaseui.auth.CredentialHelper.ACCOUNT_CHOOSER_COM
+};
 
 @NgModule({
   imports: [
-    CommonModule, RouterModule, FlexLayoutModule, ReactiveFormsModule,
-    MenuMdcModule, WebStorageModule, FirebaseModule,
+    BrowserModule,
+    FormsModule,
+    RouterModule,
+    CommonModule,
+    AngularFireModule.initializeApp( environment.firebaseConfig ),
+    AngularFireAuthModule,
+    FirebaseUIModule.forRoot(firebaseUiAuthConfig),
+    MenuMdcModule, NgMatSearchBarModule,
   ],
-  declarations: [],
-  exports: [],
-  providers: [MenuService],
+  declarations: [LogInComponent, HeaderComponent, RegisterComponent],
+  exports: [LogInComponent, HeaderComponent, RegisterComponent],
+  providers: [MenuService]
 })
 export class MenuModule { }
